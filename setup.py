931498a59d2bd8ffd4338c51706b3a39a5ec0fbf
aeksei <aeksei94@gmail.com>
2021-07-30 14:36:33 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='get-receipts',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/aeksei/get-receipts',
+    license='',
+    author='aeksei',
+    author_email='aeksei94@gmail.com',
+    description='Декодер чеков с сайта nalog.ru'
+)
